da21a702b6df8848a3126449611c298bab496a66
Jyoshna <jyoshna.d.n@gmail.com>
2026-03-25 16:24:18 +0530

    upadted

--- controllers/quizController.js
+++ controllers/quizController.js
@@ -8,3 +8,18 @@ exports.createQuiz = async (req, res) => {
     res.status(500).json({ error: error.message });
   }
 };
+exports.updateQuiz = async (req, res) => {
+  try {
+    const quiz = await Quiz.findByIdAndUpdate(
+      req.params.id,
+      {
+        questions: req.body.questions
+      },
+      { new: true }
+    );
+
+    res.json(quiz);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
--- routes/quizRoutes.js
+++ routes/quizRoutes.js
@@ -4,5 +4,6 @@ const router = express.Router();
 const { createQuiz } = require("../controllers/quizController");
 
 router.post("/create", createQuiz);
+router.put("/update/:id", updateQuiz);
 
 module.exports = router;
